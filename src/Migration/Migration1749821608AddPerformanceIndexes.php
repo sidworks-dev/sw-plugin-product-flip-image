2c5bc9e5a70ccfe8cf82c663f0a0d15d54c465d2
Jelle Siderius <jelle@hypershop.nl>
2025-11-07 13:44:57 +0100

    WIP

---
+++ src/Migration/Migration1749821608AddPerformanceIndexes.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Sidworks\ProductFlipImage\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1749821608AddPerformanceIndexes extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1749821608;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $connection->executeStatement('
+                ALTER TABLE `product`
+                ADD INDEX `idx_flip_id` (`flip_id`)
+            ');
+        $connection->executeStatement('
+                ALTER TABLE `product`
+                ADD INDEX `idx_flip_id_active` (`flip_id`, `active`)
+            ');
+    }
+}
